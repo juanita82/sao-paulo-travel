e96c8bcc88a2c1613c3107e1e0be3eb8ff4755a3
robinlovelace <rob00x@gmail.com>
2016-02-08 14:52:05 +0000

    Update schools loading

--- R/load_schools_data.R
+++ R/load_schools_data.R
@@ -2,7 +2,13 @@
 
 library(raster)
 
-schools <- shapefile("../SaoPaulo/Data/escolas_SP_2008/Escolas_de_2008_georreferenciadas.shp")
+file.rename("/tmp/data/escolas_SP_2008/ESCOLAS_DE_2008_GEORREFERENCIADAS.dbf", "/tmp/data/escolas_SP_2008/Escolas_de_2008_georreferenciadas.dbf")
+schools <- shapefile("/tmp/data/escolas_SP_2008/Escolas_de_2008_georreferenciadas.shp")
 
+proj4string(schools) <- CRS("+init=epsg:4326")
+
+library(tmap)
+library(leaflet)
+leaflet() %>% addTiles() %>% addCircles(data = schools)
 plot(schools)
 head
