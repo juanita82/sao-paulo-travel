2e102ee039ea6417d9a35ea1538a4cb82fa2c617
robinlovelace <rob00x@gmail.com>
2016-06-22 08:47:56 +0100

    Update plot

--- R/routing.R
+++ R/routing.R
@@ -1,17 +1,35 @@
 # Aim: generate routes between ODs
 pkgs = c("sp", "tmap", "rgdal", "rgeos", "stplanr", "geosphere", "dplyr")
 lapply(pkgs, library, character.only = T)
-cents = read_shape("Data/aep10.shp")
-# cents = gCentroid(cents, byid = T)
+cents = read_shape("C:/Users/juanita82/Dropbox/Robin-SaoPaulo/SaoPaulo/Data/sh_mat/sh_mat.shp")
+#proj4string(cents)<-CRS("+init=EPSG:31983")
+#names(cents@data)[names(cents@data) == 'AREA_PO'] <- 'AEP_2010'
+cents = gCentroid(cents, byid = T)
 cents = spTransform(x = cents, CRSobj = CRS("+init=epsg:4326"))
 qtm(cents)
-mapview(cents)
+#mapview(cents)
 
-f = read.csv("Data/centroids_60min.csv")
+#f = read.csv("Data/centroids_60min.csv")
+
+points2odf <- function(p){
+  if(grepl(pattern = "DataFrame", class(p))){
+    geo_code <- p@data[,1]
+  } else {
+    geo_code <- p[,1]
+  }
+  df = data.frame(
+    expand.grid(geo_code, geo_code)[2:1]
+  )
+  names(df) <- c("O", "D")
+  df
+}
 
 odf = points2odf(cents)
-brtime = strptime("2016-05-26 09:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "BRT")
 
+
+
+
+brtime = strptime("2016-05-26 09:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "BRT")
 
 l = od2line(flow = f, zones = cents)
 mapview(l)
@@ -62,22 +80,26 @@ for(i in 12001:nrow(l)){
     l$duration[i] = dists$duration
     l$fare[i] = dists$fare
   },
-    error=function(e) flag<<-FALSE
+  error=function(e) flag<<-FALSE
   )
   if (!flag) next
 }
 
 ldff = l@data
-l$dist_hav_km = l$dist_hav / 1000
+ldff = read.csv("output-data/sample-time-od-out.csv")
+ldff$dist_hav_km = ldff$dist_hav / 1000
 
 # plotting
-l$dist_band = cut(l$dist_hav_km, breaks = quantile(l$dist_hav_km))
-l$color = cut(l$dist_hav_km, breaks = quantile(l$dist_hav_km),
-              labels = c("green", "blue", "orange", "red"))
-l$color = as.character(l$color)
+ldff$dist_band = cut(ldff$dist_hav_km, breaks = quantile(ldff$dist_hav_km))
+ldff$color = cut(ldff$dist_hav_km, breaks = quantile(ldff$dist_hav_km),
+              labels = c("green", "blue", "yellow", "red"))
+ldff$color = as.character(ldff$color)
 plot(ldff$dist_hav / 1000, ldff$distances / 1000,
-    ylab = "Public transport distance (km)",
-    xlab = "Euclidean distance (km)", col = l$color)
+     ylab = "Public transport distance (km)",
+     xlab = "Euclidean distance (km)", col = ldff$color, asp = 1,
+     xlim = c(0, 40), ylim = c(0, 40))
+
+
 
 
 
@@ -97,4 +119,3 @@ tm_shape(osm_tiles) +
 qtm(l, line.col = "dist_hav_km")
 
 write.csv(ldff, "output-data/sample-time-od-out.csv")
-l = read.csv("output-data/sample-time-od-out.csv")
